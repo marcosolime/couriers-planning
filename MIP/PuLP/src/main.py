c7d01203351cdf8c393c9e52e7f62ef1b1565ce3
redfox <iu4hry@gmail.com>
2023-10-10 21:22:45 +0200

    First commit of PuLP

---
+++ MIP/PuLP/src/main.py
@@ -0,0 +1,234 @@
+'''
+MCVRP in PuLP
+'''
+from pulp import LpVariable, LpProblem, LpMinimize, lpSum, LpStatusInfeasible
+from pulp import PULP_CBC_CMD
+import sys
+import time
+import json
+import numpy as np
+import numpy.ma as ma
+
+def read_data(lines):
+    m = int(lines[0].rstrip('\n'))      # n. couriers
+    n = int(lines[1].rstrip('\n'))      # n. items
+    v = n + 1                           # n. verteces
+    l = list(map(int, lines[2].rstrip('\n').split()))   # courier size
+    s = list(map(int, lines[3].rstrip('\n').split()))   # item size
+    d = np.zeros((n+1,n+1))   # distance matrix
+    for i in range(n+1):
+        d_row = list(map(int, lines[i+4].rstrip('\n').split()))
+        for j in range(n+1):
+            d[i,j] = d_row[j]
+    d = d.astype(int)
+    return m, n, v, l, s, d
+
+def low_up_bound(d, n, v):
+    # Lower bound
+    lowBound = np.min(d[-1,:-1])
+
+    # Upper bound with greedy approach (Nearest Neighbor)
+    upBound = 0
+    min_index = n
+    mask = [False for _ in range(v)]
+    mask[min_index] = True
+    next_nodes = ma.array(d[min_index,:], mask=mask) # At the beginning, everything but the depot is available
+    
+    while True:
+        upBound += next_nodes.min()         # Neigbhour not masked node with minimum distance
+        min_index = next_nodes.argmin()
+
+        mask[min_index] = True              # The node becomes masked (visited)
+        next_nodes = ma.array(d[min_index,:], mask=mask)
+
+        if np.all(mask):                            # If all the nodes are visited
+            upBound += d[min_index, n]              # we go back to depot and stop
+            break
+    
+    return lowBound, upBound
+
+'''
+Boolean matrix of size mxm (n.couriers x n.couriers)
+Returns a matrix where item at [i,j] is True if courier
+i has more capacity than courier j; False otherwise.
+'''
+def get_is_bigger_matrix(m, l):
+    is_bigger_mat = np.zeros((m,m), dtype=bool)
+    for i in range(m):
+        for j in range(m):
+            if l[i] > l[j]:
+                is_bigger_mat[i,j] = True
+    return is_bigger_mat
+
+def main(argv):
+    TIMELIMIT = 300     # We let the solver run for this amount of seconds
+    TIMEDELTA = 2       # Time window to catch sub-optimal solutions
+
+    # Check arguments
+    if len(argv) < 2:
+        print('Insufficient arguments')
+        print('Usage: docker run <image> <instXY.dat>')
+        sys.exit(1)
+    
+    # Read file
+    str_data = argv[1]
+    lines = []
+    with open('./inst/'+str_data) as data_file:
+        for line in data_file:
+            lines.append(line)
+
+    # Read data
+    m, n, v, l, s, d = read_data(lines)
+    
+    # Preprocessing
+    lowBound, upBound = low_up_bound(d, n, v)
+    is_bigger_mat = get_is_bigger_matrix(m, l)
+
+    # The model 
+    model = LpProblem("Couriers", sense=LpMinimize)
+
+    # Variables
+    x = [[[LpVariable(f"x_{i}_{j}_{k}", cat="Binary") for k in range(m)] for j in range(v)] for i in range(v)]
+    u = [LpVariable(f"u_{i}", lowBound=1, upBound=v, cat="Integer") for i in range(v)]
+    dist = [LpVariable(f"dist_{k}", lowBound=lowBound, upBound=upBound, cat="Integer") for k in range(m)]
+    tot_load = [LpVariable(f"tot_load_{k}", lowBound=min(s), upBound=max(l)) for k in range(m)]
+    z = LpVariable("z", lowBound=lowBound, upBound=upBound, cat="Integer")
+
+    # (Objective)
+    model.setObjective(z)
+
+    # (Solver)
+    solver = PULP_CBC_CMD(timeLimit=TIMELIMIT)
+
+    # Constraints
+
+    # Keep track of the distance travelled by each courier
+    for k in range(m):
+        model += tot_load[k] == lpSum([x[i][j][k]*s[j] for i in range(v) for j in range(n)])
+
+    # (0) Avoid looping aroung the same node
+    for i in range(v):
+        for k in range(m):
+            model += x[i][i][k] == 0
+
+    # (1) Each node is visited once
+    for j in range(n):
+        model += lpSum([x[i][j][k] for k in range(m) for i in range(v)]) == 1
+    '''
+    # Redundant constraint
+    for i in range(n):
+        model += lpSum([x[i][j][k] for k in range(m) for j in range(v)]) == 1
+    '''
+        
+    # (2) Each vehicle depart and arrives at the depot
+    for k in range(m):
+        model += lpSum([x[n][j][k] for j in range(n)]) == 1   # leave once
+        model += lpSum([x[i][n][k] for i in range(n)]) == 1   # arrive once
+    
+    # (3) If we reach a delivery point, we leave it with the same courier
+    for k in range(m):
+        for j in range(v):
+            model += lpSum([x[i][j][k] for i in range(v)]) == lpSum([x[j][i][k] for i in range(v)])
+
+    # (4) Load constraint
+    for k in range(m):
+        model += tot_load[k] <= l[k]
+
+    # (5) Subtour elimination with Big-M trick
+    M = 1e5
+    model += u[n] == 1
+    for k in range(m):
+        for j in range(n):
+            for i in range(n):
+                model += u[i] + 1 <= u[j] + M*(1-x[i][j][k])
+                model += u[i] + 1 >= u[j] - M*(1-x[i][j][k])
+
+    '''
+    # This is the alternative non linear constraint
+    model += u[n] == 1
+    for i in range(v):
+        for j in range(n):
+            for k in range(m):
+                model += x[i][j][k] * u[j] >= x[i][j][k] * (u[i]+1)
+    '''
+
+    # (Sym break 1)
+    # (Couriers with more capacity deliver more weight than smaller or equal couriers)
+    for k1 in range(m):
+        for k2 in range(m):
+            if k1!=k2 and is_bigger_mat[k1,k2]:
+                model += tot_load[k1] >= tot_load[k2]
+
+
+    # (6) For each courier we calculate their distance
+    for k in range(m):
+        model += lpSum([x[i][j][k]*d[i,j] for i in range(v) for j in range(v)]) == dist[k]
+        model += z >= dist[k]
+
+    # (Solving)
+    start_time = time.time()
+    model.solve(solver)
+    elapsed_time = time.time() - start_time
+
+    # (Printing the status)
+    is_optimal = True
+    if elapsed_time >= TIMELIMIT-TIMEDELTA:
+        is_optimal = False
+
+    if model.status != LpStatusInfeasible:
+        print(f'Elapsed time: {round(elapsed_time)} second(s)')
+        if is_optimal:
+            print(f"Optimal solution found")
+        else:
+            print(f"Feasible solution found, but may be sub-optimal")
+    else:
+        print("Problem is either infeasible, unbounded or undefined")
+        sys.exit(1)
+
+    # (Gathering the routes)
+    sol = []
+    for k in range(m):
+        # Arcs in the form [(start, end), (start,end), ...]
+        active_arcs = [(i+1,j+1) for i in range(v) for j in range(v) if x[i][j][k].varValue == 1]
+        tmp_nodes = []
+        # Find first node
+        for arc in active_arcs:
+            if arc[0] == n+1:
+                next_node = arc[1]
+                tmp_nodes.append(next_node)
+                break
+        depot_reached = False
+        while not depot_reached:
+            for arc in active_arcs:
+                if arc[0] == next_node:
+                    next_node = arc[1]
+                    # End of lap
+                    if next_node == n+1:
+                        depot_reached = True
+                        break
+                    tmp_nodes.append(next_node)
+        sol.append(tmp_nodes)
+
+    # (Dumping to json)
+    to_json = {}
+    to_json['cbc'] = {}
+    to_json['cbc']['time'] = round(elapsed_time)
+    to_json['cbc']['optimal'] = is_optimal
+    to_json['cbc']['obj'] = z.varValue
+    to_json['cbc']['sol'] = sol
+
+    str_data = sys.argv[1].split('.')[0] + '.json'
+    with open('./res/' + str_data, 'w') as json_file:
+        json.dump(to_json, json_file, indent=4)
+    
+    print(to_json)
+    print('Solutions dumped to json in res folder')
+
+    # (Debugging)
+    print(f'N. of couriers = {m}')
+    for k in range(m):
+        print(f'courier {k} with total load {tot_load[k].varValue}, available {l[k]}, load occupancy {(tot_load[k].varValue/l[k])*100:.2f}%')
+
+
+if __name__ == '__main__':
+    main(sys.argv)
--- Readme.md
+++ Readme.md
@@ -1,7 +1,7 @@
 # Vehicle Routing Problem
 
 ## Problem Description
-We are given a fleet of couriers and a set of items that must be distributed to the relative clients. All the couriers start at the same location (depot), whereas the customers are located in different positions on a 2D flat region. Each courier comes with a fixed capacity, meaning that we can stuff items till a given threshold; each item has its relative weight.
+We are given a fleet of couriers and a set of items that must be distributed to a set of clients. All the couriers start at the same location (depot), whereas the customers are located in different positions on a 2D flat region. Each courier comes with a fixed capacity, meaning that we can stuff items till a given threshold; each item has its relative weight.
 The goal of the problem is to plan a route for each courier, reaching all the customers while not exceeding the courier capacities. To be fair, we want to minimize the longest distance travelled by any courier. This way, we expect to have a balanced division among the drivers in term of traveled distance.
 
 ## Implementation
@@ -14,13 +14,72 @@ To deal with this hard problem (actually, NP-Hard!), we propose three different 
 You can find the code implementation, data files and results in the correponding directories. All the folders share the same structure.
 
 - [CP/SMT/MIP]
-	* [inst]					% instances (.dat)
-	* [res]						% results (.json)
-	* [src]						% code (.py, .mzn)
+	* [inst]
+	* [res]
+	* [src]
 	* Dockerfile
-	* Readme.md
 	* requirements.txt
 
-To avoid any incompatibilies or library depencencies, we "containerized" the applications in a Docker environment. To build and run such containers, you can find the instructions in the specific subfolders.
+In the inst folder there are the problem instances; in res folder there are the results in json format; in src folder you can find the model implementations. 
 
+To avoid any incompatibilies or library depencencies, we "containerized" the applications in a Docker environment. After running the docker image, you can find the results in json format in the 'res' folder inside Docker Desktop.
+
+## CP - Constraint Programming
+We modeled our problem using MiniZinc, and solved it with Gecode and Chuffed. Additionally, we exploited several search strategies to increase convergence speed.
+
+### How to run the program
+1. Build the image: `docker build -t <image-name> ./CP`
+2. Run the image: `docker run <image-name> <solver-name> <instance-name>`
+
+Eg: `docker run mini-img gecode inst01.dat`
+Eg: `docker run mini-img chuffed inst01.dat`
+
+## SMT - Satifiability Modulo Theory
+We created the model using Z3Py Python library, and solved it with Z3.
+
+### How to run the program
+1. Build the image: `docker build -t <image-name> ./SMT`
+2. Run the image: `docker run <image-name> <instance-name>`
+
+Eg: `docker run z3-img inst01.dat`
+Eg: `docker run z3-img inst01.dat`
+
+## MIP - Mixed Integer Linear Programming
+We explored two alternatives:
+1. Gurobi API + Gurobi solver (with an Academic Licence)
+2. PuLP + Cbc solver (no license needed)
+
+### How to run the program
+1. Build the image: `docker build -t <image-name> ./MIP/{Gurobi, PuLP}`
+2. Run the image: `docker run <image-name> <instance-name>`
+
+Eg: `docker run gurobi-img inst01.dat`
+Eg: `docker run pulp-img inst01.dat`
+
+## TODOs
+
+- CP
+	1. Generate json for instances 0-21 with no simmetry constraints using Chuffed
+	2. Generate json for instances 0-21 with no simmetry constraints using Gecode
+
+- MIP
+	* Gurobi API
+		1. Implement constraint 9 (couriers with the same capacity do different routes)
+		2. Generate json for instances 0-21 with simmetry constraints
+		3. Generate json for instances 0-21 without simmetry constraints
+
+	* PuLP
+		1. Implement constraint 9 (couriers with the same capacity do different routes)
+		2. Generate json for instances 0-21 with simmetry constraints
+		3. Generate json for instances 0-21 without simmetry constraints
+
+- SMT
+	1. Write the model in Z3Py
+	2. Generate json for instances 0-21 with simmetry constraints
+	3. Generate json for instances 0-21 without simmetry constraints
+
+- Other stuff
+	* Write the project repot (Overleaf)
+	* Ideas on Warm Start
+	* Checking/Improving objective bounds
 
